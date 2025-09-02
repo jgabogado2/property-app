444b63aec905636e802419e4ebcc38612baff375
jgabogado2 <jgabogado2@student.apc.edu.ph>
2025-09-02 18:59:05 +0800

    Added Featured Properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </div>
   );
